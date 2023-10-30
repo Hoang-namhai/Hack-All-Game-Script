ecc4578641ab9ea95af4bb59730106a361f14d9e
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2023-10-30 19:22:55 +0700

    Hack_Royal_Match.lua

--- Hack_Royal_Match.lua
+++ Hack_Royal_Match.lua
@@ -1,22 +1,35 @@
-function Main()
-menu = gg.multiChoice({
-A .."  ", 
-" Thoát "},nil,"                   ⚡❤𝐘𝐨𝐮𝐭𝐮𝐛𝐞 :: 𝐇𝐚̉𝐢 𝐒𝐜𝐫𝐢𝐩𝐭🍀⚡\
+gg.alert("⛔  Bấm Chọn Vào 👉 ( Load Data Game ) Trước Thì Mới Hack Được Nhé  ⛔")
+HOME = 1
+on = "🔵 Load Data Game 🔵"
+off = "👇 Chọn Vào Menu Hack Để Hack 👇"
+A1 = on
+function Mainm()
+  menu = gg.choice({
+    "" .. A1 .. "",
+    "➡️ Menu Hack ⬅️",
+    "⛔ Thoát ⛔"}, nil,"                   ⚡❤Youtube :: Hải Script🍀⚡\
    ๑۩۞۩๑•❅──────✧❅✦❅✧──────❅•๑۩۞۩๑\
-                          ⚡𝐌𝐨𝐝 𝐁𝐲 𝐇𝐚̉𝐢 𝐒𝐜𝐫𝐢𝐩𝐭⚡\
+                          ⚡Mod By Hải Script⚡\
    ๑۩۞۩๑•❅──────✧❅✦❅✧──────❅•๑۩۞۩๑\
 ㅤㅤ\
 ㅤ ㅤ")
-if menu == nil then else
-if menu[1] == true then a1() end
-if menu[2] == true then a2() end
-end
-XGCK = -1
-end
-A ="【Hack Gold】 ✔" 
-function a1() 
-if A == "【Hack Gold】 ✔"then
-----Dán Code Vào Đây----
+    if menu == 4 then Exit()
+  elseif menu == nil then
+  elseif menu == 1 then
+    if A1 == on then
+       ad1(on)
+	  A1 = off
+	  gg.toast("๑Đã Load Xong✔๑")
+	Mainm()
+	else
+	loz()
+	end
+	end
+if menu==2 then Namhai() end
+if menu==3 then Exit() end
+  XGCK = -1
+  end
+function ad1(on)
 function searchValue(t,hai1,hai2)
 rt={}
 gg.setRanges(hai1)
@@ -60,78 +73,70 @@ end
 gg.setValues(tt)
 end end
 
-r=searchValue({"81920",{"6291712",-26*4}},gg.REGION_ANONYMOUS,gg.TYPE_DWORD)
-if #r == 0 then else
-searchEdit(-44*4,32,9999999)
-gg.alert("Đã Hack Gold Thành Công ✔")
-gg.toast("Đã Hack Gold Thành Công ✔")
-gg.clearResults()
-end
-A = "【Hack Gold】 ✔" 
-elseif A == "【Hack Gold】 ✔" then 
-----Dán Code Vào Đây----
-function searchValue(t,hai1,hai2)
-rt={}
-gg.setRanges(hai1)
-gg.clearResults()
+r=searchValue({"81920",{"6291712",-26*4},{"1",2*4},{"524288",3*4}},gg.REGION_ANONYMOUS,gg.TYPE_DWORD)
+local prev = gg.getListItems()
 gg.clearList()
-gg.setVisible(false)
-gg.searchNumber(t[1], hai2)
-local r = gg.getResults(99999999)
-if #r==0 then goto HoangNamHai end
-for it=2,#t do
-for i=1,#r do
-r[i].address=r[i].address+t[it][2]
-end
-local rr=gg.getValues(r)
-tt={}
-for i=1,#rr do
-   if rr[i].value== t[it][1] then
-   ii=#tt+1
-   tt[ii]={}
-   tt[ii].address=rr[i].address-t[it][2]
-   tt[ii].flags=4
-   end
-end
-if #tt==0 then goto HoangNamHai end
-r=gg.getValues(tt)
-if it==#t then rt=r goto HoangNamHai end
-end
-::HoangNamHai::
-return rt
-end
-
-function searchEdit(hnh1,hnh2,hnh3)
-if #r>0 then
-tt={}
-for i=1,#r do
-ii=#tt+1 tt[ii]={}
-tt[ii].address=r[i].address +hnh1
-tt[ii].flags=hnh2
-tt[ii].value=hnh3
-end
-gg.setValues(tt)
-end end
-
-r=searchValue({"81920",{"6291712",-26*4}},gg.REGION_ANONYMOUS,gg.TYPE_DWORD)
-if #r == 0 then else
-searchEdit(-44*4,32,9999999)
-gg.alert("Đã Hack Gold Thành Công ✔")
-gg.toast("Đã Hack Gold Thành Công ✔")
+local t = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.addListItems(t)
+t = nil
+gg.saveList("/sdcard/Android/data/HackRoyalMatch.txt", 0)
+gg.clearList()
+gg.addListItems(prev)
+prev = nil
 gg.clearResults()
 end
-A ="【Hack Gold】 ✔" 
+
+
+function loz()
+lozz = gg.choice({
+"๑Load Lại๑",
+"Hủy"
+}, nil, '🔰☛Bạn Muốn Load Lại Menu Không☚🔰')
+if lozz== nil then Mainm() else
+if lozz==1 then ad1()
+gg.toast('🍀Đang Load Lại....') end
+if lozz==2 then Mainm() end
 end
+lozz=-1
 end
-function a2()
+
+
+function Namhai()
+mpp = gg.multiChoice({
+"〖๑Hack Gold๑〗 ✅", 
+"⬅️ Quay Lại⬅️"
+}, nil,'๑۩۞۩๑MOD MEMU VIP๑۩۞۩๑')
+if mpp == nil then else
+if mpp[1] == true then F1() end 
+if mpp[2] == true then Exit() end
+
+end
+mpp=-1
+end
+
+
+function F1()
+gg.clearResults()
+gg.loadList("/sdcard/Android/data/HackRoyalMatch.txt", 0)
+pi = gg.getListItems()
+gg.loadResults(pi)
+gg.removeListItems(pi)
+t = searchEdit(-44*4,32,99999999)
+gg.alert("Hack Thành Công😍")
+end
+
+
+function Exit()
+
 os.exit()
 end
-while true do
+while(true)do
 if gg.isVisible(true) then
-XGCK = 1
+XGCK=1
 gg.setVisible(false)
 end
-if XGCK == 1 then Main() end
+gg.clearResults()
+if XGCK==1 then
+Mainm()
 end
-
-
+end  
