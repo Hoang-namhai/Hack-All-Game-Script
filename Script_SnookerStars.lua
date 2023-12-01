2b7af21e2f63588d9b1575a88bdec887a7b2f8c3
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2023-12-01 18:25:24 +0700

    Update Script_SnookerStars.lua

--- Script_SnookerStars.lua
+++ Script_SnookerStars.lua
@@ -1,5 +1,6 @@
 gg.alert('❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀 ᴠᴇʀsɪᴏɴ 1.0.0\
 🔥Zᴀʟᴏ: 0358184454📲 ┆Mᴇɴᴜ Hᴀᴄᴋ Sɴᴏᴏᴋᴇʀ Sᴛᴀʀs')
+gg.clearList()
 gg.searchNumber("1019191344", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
 local t = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
 gg.addListItems(t)
@@ -8,35 +9,7 @@ local copy = false
 local t = gg.getListItems()
 if not copy then gg.removeListItems(t) end
 for i, v in ipairs(t) do
-	v.address = v.address + 0xffffffffffffffec
-	if copy then v.name = v.name..' #2' end
-end
-gg.addListItems(t)
-t = nil
-copy = nil
-revert = gg.getListItems()
-local t = gg.getListItems()
-for i, v in ipairs(t) do
-	if v.flags == gg.TYPE_DWORD then
-		v.value = "8888"
-		v.freeze = true
-		v.freezeType = gg.FREEZE_NORMAL
-	end
-end
-gg.addListItems(t)
-t = nil
-gg.processResume()
-gg.clearResults()
-gg.searchNumber("1019191344", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-
-local t = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
-gg.addListItems(t)
-t = nil
-local copy = false
-local t = gg.getListItems()
-if not copy then gg.removeListItems(t) end
-for i, v in ipairs(t) do
-	v.address = v.address + 0xffffffffffffffec
+	v.address = v.address + 0xffffffffffffffe4
 	if copy then v.name = v.name..' #2' end
 end
 gg.addListItems(t)
