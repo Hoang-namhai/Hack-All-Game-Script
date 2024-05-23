e6e961c2282a355066bcccb975b0d39b2b743f2c
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2024-05-23 21:40:19 +0700

    Update TiaDaiV1.lua

--- TiaDaiV1.lua
+++ TiaDaiV1.lua
@@ -1,3 +1,3 @@
 gg.alert("☛Phiên Bản Hack Free Hiện Tại Đã Hết Hạn☚")
 gg.alert("☛Liên Hệ ADMIN Hoặc Đợi Bản Free Mới Để Sử Dụng☚")
-gg.alert("☛Zalo ADMIN: 0358184454 Chuyên Tool Hack Bida☚")
+gg.alert("☛Zalo ADMIN: 03575574755 Chuyên Tool Hack Bida☚")
