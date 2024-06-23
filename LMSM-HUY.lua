4da06e999869d58caa9977ebfd466b0736c4a7b2
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2024-06-23 21:16:25 +0700

    Update LMSM-HUY.lua

--- LMSM-HUY.lua
+++ LMSM-HUY.lua
@@ -1,46 +1,70 @@
 Zzz = 0
 function HOME()
 MENU = gg.choice({
-"🔘  Search Value 💕",
-"🔘  [EXIT]"},nil,"❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀 ᴠᴇʀsɪᴏɴ 1.1.642\
+"🔘  One Hit 💥",
+"🔘  God Mod 💕",
+"🔘  [EXIT]"},nil,"❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀 ᴠᴇʀsɪᴏɴ 1.1.691\
 🔥Zᴀʟᴏ: 0375574755📲 ┆Mᴇɴᴜ Hᴀᴄᴋ Liên Minh Stickman!\
   ")
 if MENU == nil then
 else
-if MENU == 1 then SEARCH() end
-if MENU == 2 then EXIT() end
+if MENU == 1 then ONEHIT() end
+if MENU == 2 then GODMOD() end
+if MENU == 3 then EXIT() end
 end
 LOL = -1
 end
 
 
 
-
-function SEARCH()
+function ONEHIT()
 gg.clearResults()
 gg.setRanges(gg.REGION_ANONYMOUS)
 gg.clearResults()
 if input == nil then
-HNH = "☛☚"
+HNH = ' "Số\nDamage"\n\n\n;\n\n\n"Số\n%"\n\n\n\n\n\n\n00:17" '
 else
 HNH = input[1]
 end
 input = gg.prompt(
-{"🔰Nhập Giá Trị Hack🔰:"},
+{"🔰Nhập Thông Số Giá Trị Trên Trang Bị🔰:"},
 {HNH},  
 {"number"})
 if input and input[1] then
 local message = input[1]
-gg.searchNumber(message, gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL)
-gg.sleep(1000)
-gg.refineNumber(message, gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL)
-gg.sleep(2000)
-gg.refineNumber(message, gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL)
+gg.searchNumber(message, gg.TYPE_DWORD, false, gg.SIGN_EQUAL)
+gg.refineNumber(message, gg.TYPE_DWORD, false, gg.SIGN_EQUAL)
 revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
-gg.editAll('8.88888889e99', gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL)
+gg.editAll('888888888', gg.TYPE_DWORD, false, gg.SIGN_EQUAL)
 gg.clearResults()
 end
-gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+gg.toast('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+end
+
+
+
+function GODMOD()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+if input == nil then
+HNH = ' "Số\nMáu"\n\n\n;\n\n\n"Số\n%"\n\n\n\n\n\n\n00:17" '
+else
+HNH = input[1]
+end
+input = gg.prompt(
+{"🔰Nhập Thông Số Giá Trị Trên Trang Bị🔰:"},
+{HNH},  
+{"number"})
+if input and input[1] then
+local message = input[1]
+gg.searchNumber(message, gg.TYPE_DWORD, false, gg.SIGN_EQUAL)
+gg.refineNumber(message, gg.TYPE_DWORD, false, gg.SIGN_EQUAL)
+revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll('888888888', gg.TYPE_DWORD, false, gg.SIGN_EQUAL)
+gg.clearResults()
+end
+gg.toast('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
 end
 
 
